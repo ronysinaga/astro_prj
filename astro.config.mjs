ab88ff5529aafa1141d82263a10301bb2395c719
ronysinaga <ronyataptika@gmail.com>
2025-02-06 15:18:54 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+
+import preact from "@astrojs/preact";
+
+
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      theme: 'github-light',
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-  vite: {
-    server: {
-      fs: {
-        // Handle UTF-8 with BOM
-        strict: false
-      }
-    }
-  }
+  site: "https://example.com",
+  integrations: [preact()]
 });
